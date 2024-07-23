3185377770e86cc1dd2fc901d97b0788977df519
TheDiegster <dam001ucsd.edu>
2024-07-23 13:30:14 -0700

    first working event!

--- readPdf.py
+++ readPdf.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 from ics import Calendar, Event
 import pytz
 from pypdf import PdfReader
-
+from ics.grammar.parse import ContentLine
 # All the possible course codes at UCSD
 rawCourseCodes = ["AAPI", "AAS", "AESE", "AIP", "ANAR", "ANBI", "ANES", "ANSC", "ANTH", "ASTR", "AUD", "AWP", "BENG", "BGGN", "BGJC", "BGRD", "BGSE", "BIBC", "BICD", "BIEB", "BILD", "BIMM", "BIOM", "BIPN", "BISP", "BNFO", "CAT", "CCE", "CCS", "CENG", "CGS", "CHEM", "CHIN", "CLAS", "CLIN", "CLRE", "CLSS", "CLX", "CMM", "COGR", "COGS", "COMM", "CONT", "CSE", "CSS", "DDPM", "DERM", "DOC", "DSC", "DSE", "DSGN", "EAP", "ECE", "ECON", "EDS", "EIGH", "EMED", "ENG", "ENVR", "ERC", "ESYS", "ETHN", "ETIM", "EXPR", "FILM", "FMPH", "FPM", "GLBH", "GMST", "GPCO", "GPEC", "GPGN", "GPIM", "GPLA", "GPPA", "GPPS", "GSS", "HDP", "HDS", "HIAF", "HIEA", "HIEU", "HIGL", "HIGR", "HILA", "HILD", "HINE", "HISA", "HISC", "HITO", "HIUS", "HLAW", "HMNR", "HUM", "ICEP", "INTL", "IRLA", "JAPN", "JWSP", "LATI", "LAWS", "LHCO", "LIAB", "LIDS", "LIEO", "LIFR", "LIGM", "LIGN", "LIHI", "LIHL", "LIIT", "LIPO", "LISL", "LISP", "LTAF", "LTAM", "LTCH", "LTCO", "LTCS", "LTEA", "LTEN", "LTEU", "LTFR", "LTGK", "LTGM", "LTIT", "LTKO", "LTLA", "LTRU", "LTSP", "LTTH", "LTWL", "LTWR", "MAE", "MATH", "MATS", "MBC", "MCWP", "MDE", "MED", "MGT", "MGTA", "MGTF", "MGTP", "MMW", "MSED", "MSP", "MUIR", "MUS", "NANO", "NEU", "NEUG", "OBG", "OPTH", "ORTH", "PATH", "PEDS", "PH", "PHAR", "PHB", "PHIL", "PHLH", "PHYA", "PHYS", "POLI", "PSY", "PSYC", "RAD", "RELI", "REV", "RMAS", "RMED", "SE", "SEV", "SIO", "SIOB", "SIOC", "SIOG", "SOCE", "SOCG", "SOCI", "SOCL", "SOMC", "SOMI", "SPPH", "SPPS", "SURG", "SXTH", "SYN", "TDAC", "TDDE", "TDDM", "TDDR", "TDGE", "TDGR", "TDHD", "TDHT", "TDMV", "TDPF", "TDPR", "TDPW", "TDTR", "TKS", "TMC", "TWS", "UROL", "USP", "VIS", "WARR", "WCWP", "WES"]
 courseCodes = set(rawCourseCodes)
@@ -92,8 +92,8 @@ for row in selectRows:
         cleanCols.append(element.strip())
     finalFormattedRows.append(cleanCols)
         
-for row in finalFormattedRows:
-    print(row)
+# for row in finalFormattedRows:
+#     print(row)
 
 df = pd.DataFrame(finalFormattedRows)
 
@@ -160,19 +160,68 @@ else:
     endHour = endTime[:-4]
     endMinute = endTime[-2:]
 
+#print("START " + startTime)
+#print("END " + endTime)
 startMinFirst = int(startMinute[0])
 startMinSecond = int(startMinute[1])
 endMinFirst = int(endMinute[0])
 endMinSecond = int(endMinute[1])
-print(startTime)
-print(endTime)
+#print("this is end minute: " + endMinute)
 #lecture.begin = dt(2023, 7, 20, 9, 0, 0, tzinfo=pytz.timezone("America/Los_Angeles"))
 
-lecture.begin = dt(2024, 1, 3, int(startHour), startMinFirst, startMinSecond, tzinfo=pytz.timezone("America/Los_Angeles"))
+def formatDays(days):
+    formattedDays = []
+    for char in days:
+        if char == 'M':
+            formattedDays.append('MO')
+        elif char == 'Tu':
+            formattedDays.append('TU')
+        elif char == 'W':
+            formattedDays.append('WE')   
+        elif char == 'Th':
+            formattedDays.append('TH')
+        elif char == 'F':
+            formattedDays.append('FR')
+    return formattedDays
+
+untilDt = dt(2024, 3, 15, 23, 59)
+pacific = pytz.timezone("America/Los_Angeles")
+untilPacDt = pacific.localize(untilDt)
+untilUtcDt = untilPacDt.astimezone(pytz.utc)
+untilString = untilUtcDt.strftime('UNTIL=%Y%m%dT%H%M%SZ')
+
+formattedDays = formatDays(finalFormattedRows[0][7])
+#print(formattedDays)
+#lecture.begin = dt(2024, 1, 8, int(startHour), int(startMinute), tzinfo=pytz.timezone("America/Los_Angeles"))
+dtStart = dt(2024, 1, 8, int(startHour), int(startMinute))
+dtStartIcs = dtStart.strftime('%Y%m%dT%H%M%S')
+dtEnd = dt(2024, 1, 8, int(endHour), int(endMinute))
+dtEndIcs = dtEnd.strftime('%Y%m%dT%H%M%S')
+
+dateToDay = dtStart.weekday()
+daysOfTheWeek = ["MO","TU", "WE", "TH", "FR"]
+startDay = daysOfTheWeek[dateToDay]
 # we need to find a way to do repeat until!
-lecture.end = dt(2024, 1, 3, int(endHour), endMinFirst, endMinSecond, tzinfo=pytz.timezone("America/Los_Angeles"))
+# lecture.end = dt(2024, 1, 8, int(endHour), int(endMinute), tzinfo=pytz.timezone("America/Los_Angeles"))
+# created sets DTSTAMP which is required
+#print(lecture.end)
+lecture.created = dt.now(pytz.utc)
+
+byDayString = ','.join(formattedDays)
+rruleString = "FREQ=WEEKLY;" + untilString + ";WKST=" + "SU" + ";BYDAY=" + byDayString
+lecture.extra.append(ContentLine(name="RRULE", value=rruleString))
+
+
+
+lecture.extra.append(ContentLine("DTSTART", {'TZID': ['America/Los_Angeles']}, dtStartIcs))
+lecture.extra.append(ContentLine("DTEND", {'TZID': ['America/Los_Angeles']}, dtEndIcs))
+
+
 # lecture.begin = '2014-01-01 00:00:00'
 c.events.add(lecture)
-print(c.serialize())
+
+with open('output.ics', 'w') as outputFile:
+        outputFile.writelines(c.serialize_iter())
+
 
 
