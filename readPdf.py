f90152a95b2c9faef38bb7cda56bacfb8a6fa3b7
TheDiegster <dam001ucsd.edu>
2024-07-23 14:56:58 -0700

    refactor #1

--- readPdf.py
+++ readPdf.py
@@ -18,8 +18,9 @@ start_index = 4
 end_index = -1
 selectRows = rawRows[start_index:end_index]
 
-# for item in selectRows:
-#     print(item)
+def printPdfFormat():
+    for item in selectRows:
+        print(item)
 
 noLeadSpaceRows = [row.strip() for row in selectRows]
 cleanedRows = [' '.join(row.split()) for row in noLeadSpaceRows]
@@ -91,42 +92,29 @@ for row in selectRows:
     for element in cols:
         cleanCols.append(element.strip())
     finalFormattedRows.append(cleanCols)
-        
-# for row in finalFormattedRows:
-#     print(row)
+
+def printCsvFormat():        
+    for row in finalFormattedRows:
+        print(row)
 
 df = pd.DataFrame(finalFormattedRows)
 
-#print(df)
-
-# Next Steps: Create a .ics file based on our dataframe or list of lists, use python ics lib
-
-c = Calendar()
-lecture = Event()
-discussion = Event()
-midtermOne = Event()
-midtermTwo = Event()
-# for test purposes let's create a MATH 20D calendar
-
-lecture.name = finalFormattedRows[0][0] + " Lecture"
-
-# will have to prompt user for beginning of instruction date
+# TODO: prompt user for beginning/end of instruction date
 # for now use hard coded start and end days
-# it seems UCSD updates class schuedule yearly so just do that
-# for this example we have jan 3rd to march 15th 2024, prob put these in variables later
-#e.begin = datetime(2023, 7, 20, 7, 0, 0, tzinfo=pytz.timezone("America/Los_Angeles"))
-startTime = ''
-endTime = ''
-sawDash = False
-timeString = finalFormattedRows[0][8]
-for char in timeString:
-    if sawDash == True:
-        endTime += char
-        continue
-    if char == '-':
-        sawDash = True
-        continue
-    startTime += char
+
+def createStartAndEndTimes(timeString):
+    startTime = ''
+    endTime = ''
+    sawDash = False
+    for char in timeString:
+        if sawDash == True:
+            endTime += char
+            continue
+        if char == '-':
+            sawDash = True
+            continue
+        startTime += char
+    return startTime, endTime
 
 def standardToMilitaryTime(timeToConvert):
     if timeToConvert[0] != 0:
@@ -137,38 +125,38 @@ def standardToMilitaryTime(timeToConvert):
     if timeToConvert[-1] == 'p':
         timeToConvert = timeToConvert[:-1]
         timeToConvert = timeToConvert + "PM"
+
     standardTime = dt.strptime(timeToConvert, '%I:%M%p')
     militaryTime = standardTime.strftime('%H:%M')
     return militaryTime
 
-startHour = ''
-startMinute = ''
-endHour = ''
-endMinute = ''
-if startTime[-1] == 'p':
-    startTime = standardToMilitaryTime(startTime)
-    startHour = startTime[:-3]
-    startMinute = startTime[-2:]
-else:
-    startHour = startTime[:-4]
-    startMinute = startTime[-2:]
-if endTime[-1] == 'p':
-    endTime = standardToMilitaryTime(endTime)
-    endHour = endTime[:-3]
-    endMinute = endTime[-2:]
-else:
-    endHour = endTime[:-4]
-    endMinute = endTime[-2:]
+def formatStartTime(startTime):
+    startHour = ''
+    startMinute = ''
 
-#print("START " + startTime)
-#print("END " + endTime)
-startMinFirst = int(startMinute[0])
-startMinSecond = int(startMinute[1])
-endMinFirst = int(endMinute[0])
-endMinSecond = int(endMinute[1])
-#print("this is end minute: " + endMinute)
-#lecture.begin = dt(2023, 7, 20, 9, 0, 0, tzinfo=pytz.timezone("America/Los_Angeles"))
+    if startTime[-1] == 'p':
+        startTime = standardToMilitaryTime(startTime)
+        startHour = startTime[:-3]
+        startMinute = startTime[-2:]
+    else:
+        startHour = startTime[:-4]
+        startMinute = startTime[-2:]
+    return startHour, startMinute
 
+def formatEndTime(endTime):
+    endHour = ''
+    endMinute = ''
+    if endTime[-1] == 'p':
+        endTime = standardToMilitaryTime(endTime)
+        endHour = endTime[:-3]
+        endMinute = endTime[-2:]
+    else:
+        endHour = endTime[:-4]
+        endMinute = endTime[-2:]
+    return endHour, endMinute
+
+# INPUT: string of WebReg days of a certain event
+# OUTPUT: string of WebReg days in .ics readable format
 def formatDays(days):
     formattedDays = []
     for char in days:
@@ -184,44 +172,55 @@ def formatDays(days):
             formattedDays.append('FR')
     return formattedDays
 
+# use .ics lib to create a calendar object in which events can go into
+c = Calendar()
+lecture = Event()
+discussion = Event()
+midtermOne = Event()
+midtermTwo = Event()
+
+# hard coded lecture name, and start/end time
+lecture.name = finalFormattedRows[0][0] + " Lecture"
+timeString = finalFormattedRows[0][8]
+
+# create datetime strings for DTSTART and DTEND fields
+startTime, endTime = createStartAndEndTimes(timeString)
+startHour, startMinute = formatStartTime(startTime)
+dtStart = dt(2024, 1, 8, int(startHour), int(startMinute))
+startString = dtStart.strftime('%Y%m%dT%H%M%S')
+
+endHour, endMinute = formatEndTime(endTime)
+dtEnd = dt(2024, 1, 8, int(endHour), int(endMinute))
+endString = dtEnd.strftime('%Y%m%dT%H%M%S')
+
+# create datetime string for UNTIL field, currently using hard coded end of
+# instruction date.
 untilDt = dt(2024, 3, 15, 23, 59)
 pacific = pytz.timezone("America/Los_Angeles")
 untilPacDt = pacific.localize(untilDt)
 untilUtcDt = untilPacDt.astimezone(pytz.utc)
 untilString = untilUtcDt.strftime('UNTIL=%Y%m%dT%H%M%SZ')
 
+# create string for the BYDAY field
 formattedDays = formatDays(finalFormattedRows[0][7])
-#print(formattedDays)
-#lecture.begin = dt(2024, 1, 8, int(startHour), int(startMinute), tzinfo=pytz.timezone("America/Los_Angeles"))
-dtStart = dt(2024, 1, 8, int(startHour), int(startMinute))
-dtStartIcs = dtStart.strftime('%Y%m%dT%H%M%S')
-dtEnd = dt(2024, 1, 8, int(endHour), int(endMinute))
-dtEndIcs = dtEnd.strftime('%Y%m%dT%H%M%S')
-
-dateToDay = dtStart.weekday()
-daysOfTheWeek = ["MO","TU", "WE", "TH", "FR"]
-startDay = daysOfTheWeek[dateToDay]
-# we need to find a way to do repeat until!
-# lecture.end = dt(2024, 1, 8, int(endHour), int(endMinute), tzinfo=pytz.timezone("America/Los_Angeles"))
-# created sets DTSTAMP which is required
-#print(lecture.end)
-lecture.created = dt.now(pytz.utc)
-
 byDayString = ','.join(formattedDays)
+
+# create final rruleString
 rruleString = "FREQ=WEEKLY;" + untilString + ";WKST=" + "SU" + ";BYDAY=" + byDayString
+
+# add all field information the lecture event
+lecture.name = finalFormattedRows[0][0] + " Lecture"
+lecture.created = dt.now(pytz.utc)
 lecture.extra.append(ContentLine(name="RRULE", value=rruleString))
+lecture.extra.append(ContentLine("DTSTART", {'TZID': ['America/Los_Angeles']}, startString))
+lecture.extra.append(ContentLine("DTEND", {'TZID': ['America/Los_Angeles']}, endString))
 
-
-
-lecture.extra.append(ContentLine("DTSTART", {'TZID': ['America/Los_Angeles']}, dtStartIcs))
-lecture.extra.append(ContentLine("DTEND", {'TZID': ['America/Los_Angeles']}, dtEndIcs))
-
-
-# lecture.begin = '2014-01-01 00:00:00'
 c.events.add(lecture)
 
 with open('output.ics', 'w') as outputFile:
         outputFile.writelines(c.serialize_iter())
 
+
+# TODO: find a way to automate the creation of all events for ONE class
 
 
